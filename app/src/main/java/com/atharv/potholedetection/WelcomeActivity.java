29ee7c27265925af908c57113dad874dec47332c
GeetikaMahour <geetikamahour777@gmail.com>
2024-02-27 12:52:40 +0530

    Welcome Page create

---
+++ app/src/main/java/com/atharv/potholedetection/WelcomeActivity.java
@@ -0,0 +1,21 @@
+package com.atharv.potholedetection;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        // Set the two-color text to the TextView
+    }
+}
