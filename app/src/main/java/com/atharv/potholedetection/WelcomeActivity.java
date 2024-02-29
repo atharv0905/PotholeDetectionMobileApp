9a23274349880e4d9531c7981fee1125c592cd7f
GeetikaMahour <geetikamahour777@gmail.com>
2024-03-01 00:39:09 +0530

    Welcome Page Modified (Pothole Tracking System)

--- app/src/main/java/com/atharv/potholedetection/WelcomeActivity.java
+++ app/src/main/java/com/atharv/potholedetection/WelcomeActivity.java
@@ -1,11 +1,10 @@
 package com.atharv.potholedetection;
 
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.style.ForegroundColorSpan;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +15,19 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        // Set the two-color text to the TextView
+        Button get_started = findViewById(R.id.get_started_button);
+
+        // Set OnClickListener for the "get_started" button
+        get_started.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Button Clicked", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
+
